6c9525d9dc2bfe0d689bdac43dd97c7fbdce2a89
Juan Dominguez <juanignaciodom3@gmail.com>
2021-06-16 10:57:18 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import QrReader from "react-qr-reader";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <nav className="nav">
+          <ul className="nav-list">
+            <li className="nav-item">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return (
+    <div>
+      Escanee su codigo qr aqui
+      <QrReader
+        delay={300}
+        onError={() => console.log("b")}
+        onScan={(data) => data !== null && alert(data)}
+        style={{ width: "100%" }}
+      />
     </div>
   );
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
